0081ee306f4cf81329b1681feb6ee36262abe049
EmployingDGC <63275906+EmployingDGC@users.noreply.github.com>
2020-04-25 23:05:58 -0300

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,23 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  // Animal animal = new Animal(); // ERRO
+
+  Cachorro cachorro = new Cachorro(); // Permitido
+
+  Gato gato = new Gato(); // Permitido
+}
+
+/*
+ * Classes abistratas n podem ser instanciadas sozinhas, são
+ * usadas quando não faz sentido criar uma instancia da superclasse
+ */
+abstract class Animal {
+
+}
+
+class Cachorro extends Animal {
+
+}
+
+class Gato extends Animal {
+
 }
